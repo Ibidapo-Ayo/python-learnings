27e9c6821cd7c2c6565896a5ee9429cebfb9ad4d
Ibidapo Ayomide Victor <49324541+Ibidapo-Ayo@users.noreply.github.com>
2025-07-26 00:32:11 +0000

    Rock paper scissor game

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import random
+
+
 def get_choice():
   player_choice = input("Enter a choice (rock, paper, scissors): ")
   options = ["rock", "paper", "scissors"]
   computer_choice = random.choice(options)
   choices = {"player": player_choice, "computer": computer_choice}
   return choices
+
 
 def check_win(player, computer):
   print(f"You chose {player}, computer chose {computer}")
@@ -20,7 +23,7 @@ def check_win(player, computer):
   elif player == "rock":
     if computer == "scissors":
       return "Rock smashes scissors! You win!"
-    else: 
+    else:
       return "Paper covers rock! You lose."
   elif player == "scissors":
     if computer == "paper":
@@ -29,6 +32,7 @@ def check_win(player, computer):
       return "Rock smashes scissors! You lose."
   return [player, computer]
 
+
 choices = get_choice()
 result = check_win(choices["player"], choices["computer"])
-print(result)
+print(f"Your result is: {result}")
