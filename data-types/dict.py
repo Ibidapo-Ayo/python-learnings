6310a2a939a54cb208b626fd12aebac9f4d51294
Ibidapo Ayomide <ibidapoayomide754@gmail.com>
2025-10-24 01:41:56 +0100

    Data types learnings

--- data-types/dict.py
+++ data-types/dict.py
@@ -1,9 +1,20 @@
 # Dictionary
 
-dictionary = {
-    "a": [1,2,3],
-    "b": "hello",
-    "c": 3,
+# dictionary = {
+#     "a": [1,2,3],
+#     "b": "hello",
+#     "c": 3,
+# }
+#
+# print(dictionary["b"])
+
+user = {
+    "basket": [1,2,3],
+    "greet": "hello",
+    "age": 20
 }
 
-print(dictionary["b"])
+user2 = dict(name="JohnJohn")
+print(user2)
+
+print(user.clear())
---
+++ data-types/tuple.py
@@ -0,0 +1,2 @@
+new_tuple = (1, 2, 3)
+print(new_tuple.count(1))
